d2c7817bcb7dc96b6df9bded57472d92f0ca8487
Shubham Khaskel <khaskelshubham@gmail.com>
2022-02-04 20:59:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Welcome to Shubham's music");
+let songs=[
+    {songName: "Lambiyaan", filePath:"D:\project-spotify", coverPath:"D:\project-spotify"}
+]
+let audioelement=new Audio('1.mp3');
+audioelement.play();
